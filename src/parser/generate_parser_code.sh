5bdc884a2cb573ceed731e54b2d59fe692f96fad
Jaroslaw Staniek <staniek@kde.org>
2008-08-12 21:57:22 +0000

    parser: camel case
    svn path=/branches/work/predicate/; revision=846067

--- src/parser/generate_parser_code.sh
+++ src/parser/generate_parser_code.sh
@@ -3,21 +3,21 @@
 
 dir=`dirname $0`
 cd $dir
-flex -osqlscanner.cpp sqlscanner.l
-bison -dv sqlparser.y
+flex -oSqlScanner.cpp SqlScanner.l
+bison -dv SqlParser.y
 echo '#ifndef _SQLPARSER_H_
 #define _SQLPARSER_H_
-#include <kexidb/field.h>
-#include "parser.h"
-#include "sqltypes.h"
+#include "Field.h"
+#include "Parser.h"
+#include "SqlTypes.h"
 
-bool parseData(KexiDB::Parser *p, const char *data);' > sqlparser.h
+bool parseData(KexiDB::Parser *p, const char *data);' > SqlParser.h
 
-cat sqlparser.tab.h >> sqlparser.h
-echo '#endif' >> sqlparser.h
+cat SqlParser.tab.h >> SqlParser.h
+echo '#endif' >> SqlParser.h
 
-cat sqlparser.tab.c > sqlparser.cpp
-echo "const char * const tname(int offset) { return yytname[offset]; }" >> sqlparser.cpp
+cat SqlParser.tab.c > SqlParser.cpp
+echo "const char * const tname(int offset) { return yytname[offset]; }" >> SqlParser.cpp
 
 ./extract_tokens.sh > tokens.cpp
-rm -f sqlparser.tab.h sqlparser.tab.c
+rm -f SqlParser.tab.h SqlParser.tab.c
