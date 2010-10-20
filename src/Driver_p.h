a03a66a8fc88d4073a3ba6e2cd7f31bab314cf6d
Jaroslaw Staniek <staniek@kde.org>
2010-10-20 20:52:15 +0000

    more explanation for the boolean literals SVN_SILENT
    svn path=/branches/work/predicate/; revision=1187949

--- src/Driver_p.h
+++ src/Driver_p.h
@@ -137,10 +137,16 @@ public:
      Used in Connection::resultExists() for optimization. It's set to true for SQLite driver. */
     bool SELECT_1_SUBQUERY_SUPPORTED;
 
-    /*! Literal for boolean true. "1" by default. */
+    /*! Literal for boolean true. "1" by default
+        which is typically expected by backends even while the standard says "TRUE":
+        http://troels.arvin.dk/db/rdbms/#data_types-boolean
+    */
     QString BOOLEAN_TRUE_LITERAL;
 
-    /*! Literal for boolean false. "0" by default. */
+    /*! Literal for boolean false. "0" by default
+        which is typically expected by backends even while the standard says "TRUE":
+        http://troels.arvin.dk/db/rdbms/#data_types-boolean
+    */
     QString BOOLEAN_FALSE_LITERAL;
 };
 
