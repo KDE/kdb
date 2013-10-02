e5f620d2f585acaab29406a53ac0f9fc6ac6bcb5
Jaroslaw Staniek <staniek@kde.org>
2013-10-02 12:56:06 +0200

    proper indent, make array const

--- src/parser/extract_tokens.sh
+++ src/parser/extract_tokens.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "/* WARNING! All changes made in this file will be lost! Run 'generate_parser_code.sh' instead. */
-static const char* _tokens[] = {"
+static const char* const _tokens[] = {"
 for t in `grep  "\"[a-zA-Z_]*\"" SqlScanner.l | sed -e "s/\(^[^\"]*\)\"\([^\"]*\)\".*$/\2/g" | sort | uniq` ; do
 	if [ "$t" = "ZZZ" ] ; then break ; fi
-	echo "\"$t\",";
+    echo -e "    \"$t\",";
 done
 
-echo "0
+echo "    0
 };"
