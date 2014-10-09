6b9fd1f34a9f1aaa06aee434e75dfd99cfebdcd3
Jaroslaw Staniek <staniek@kde.org>
2014-10-09 22:04:59 +0200

    Update config helper scripts: instructions, rm unused args

--- tools/config-for-kde.sh
+++ tools/config-for-kde.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 #
 # Generates build files using CMake with Debug enabled and re-using
-# KDE 4 directory $KDEDIRS.
+# KDE 4 directory pointed by the $KDEDIRS environment variable.
 #
 # Usage:
-#  0. Set $KDEDIRS
-#  1. mkdir -p {predicate-build-dir}
-#  2. cd {predicate-build-dir}
-#  3. {predicate-source-dir}/tools/config-for-kde.sh
-#
+#  0. Set $KDEDIRS environment variable to directory of your local
+#     KDE installation (by convention it is $HOME/kde4/inst)
+#  1. Pick location for your Predicate build directory {predicate-build-dir} and:
+#     % mkdir -p {predicate-build-dir}
+#  2. % cd {predicate-build-dir}
+#  3. % {predicate-source-dir}/tools/config-for-kde.sh {cmake-options}
+#     ({predicate-source-dir} is your Predicate source code directory,
+#      and {cmake-options} are extra cmake options as documented at 
+#      http://www.cmake.org/cmake/help/v2.8.8/cmake.html#section_Options)
+
 # "Dry run" mode:
-#  Add --dry-run parameter. This only displays the resulting cmake command
+#  Add --dry-run option. This only displays the resulting cmake command
 #  and exists without executing cmake. Useful when cmake arguments
 #  are needed e.g. for configuring a Qt Creator project.
 #
-# You need to type the above command only once to configure the CMake-based
-# buildsystem.
+# You need to type the above commands only once to configure the CMake-based
+# buildsystem. However the script can be re-run safely.
+# To completely reconfigure, remove the {predicate-build-dir} completely before
+# running the script or cmake again.
 #
 # To build and install type:
 #  make
 #  make install
+#
+# Detailed configuration and build instructions: https://community.kde.org/Predicate/Build
 #
 
 SOURCE_DIR=$(readlink -f $(dirname $0)/..)
@@ -40,11 +49,12 @@ else
     _libdir=lib
 fi
 
-export PREDICATE_INSTALL_PREFIX=$KDEDIRS
+export PREDICATE_INSTALL_PREFIX=${KDEDIRS}
 
-cmd="cmake ${SOURCE_DIR} -DCMAKE_BUILD_TYPE=DebugFull -DCMAKE_INSTALL_PREFIX=$PREDICATE_INSTALL_PREFIX \
-    -DLIB_INSTALL_DIR=$PREDICATE_INSTALL_PREFIX/$_libdir -DPLUGIN_INSTALL_DIR=$PREDICATE_INSTALL_PREFIX/$_libdir/kde4/plugins \
-    -DQT_PLUGINS_DIR=$PREDICATE_INSTALL_PREFIX/$_libdir/kde4/plugins"
+cmd="cmake $* -DCMAKE_INSTALL_PREFIX=${PREDICATE_INSTALL_PREFIX} \
+    -DLIB_INSTALL_DIR=${PREDICATE_INSTALL_PREFIX}/${_libdir} \
+    -DPLUGIN_INSTALL_DIR=${PREDICATE_INSTALL_PREFIX}/${_libdir}/kde4/plugins \
+    -DQT_PLUGINS_DIR=$PREDICATE_INSTALL_PREFIX/${_libdir}/kde4/plugins ${SOURCE_DIR}"
 
 echo "------------------------------------------"
 echo "CMake will be executed using this command:"
--- tools/config-for-local.sh
+++ tools/config-for-local.sh
@@ -3,31 +3,42 @@
 # Generates build files using CMake with Debug enabled and local PREFIX.
 #
 # Usage:
-#  0. Set $PREDICATE_INSTALL_PREFIX, e.g. to $HOME/predicate/install
-#  1. mkdir -p {predicate-build-dir}
-#  2. cd {predicate-build-dir}
-#  3. {predicate-source-dir}/tools/config-for-local.sh {predicate-source-dir}
+#  0. Set $PREDICATE_INSTALL_PREFIX environment variable to directory that
+#      is indended PREFIX for the local Predicate installation,
+#      e.g. to $HOME/predicate/inst
+#  1. Pick location for your Predicate build directory {predicate-build-dir} and:
+#     % mkdir -p {predicate-build-dir}
+#  2. % cd {predicate-build-dir}
+#  3. % {predicate-source-dir}/tools/config-for-local.sh {cmake-options}
+#     ({predicate-source-dir} is your Predicate source code directory,
+#      and {cmake-options} are extra cmake options as documented at 
+#      http://www.cmake.org/cmake/help/v2.8.8/cmake.html#section_Options)
 #
 # "Dry run" mode:
-#  Add --dry-run parameter. This only displays the resulting cmake command
+#  Add --dry-run option. This only displays the resulting cmake command
 #  and exists without executing cmake. Useful when cmake arguments
 #  are needed e.g. for configuring a Qt Creator project.
 #
-# You need to type the above command only once to configure the CMake-based
-# buildsystem.
+# You need to type the above commands only once to configure the CMake-based
+# buildsystem. However the script can be re-run safely.
+# To completely reconfigure, remove the {predicate-build-dir} completely.
 #
 # To build and install type:
 #  make
 #  make install
 #
+# Detailed configuration and build instructions: https://community.kde.org/Predicate/Build
+#
 
-if [ $# -lt 1 ] ; then
-    echo "Usage: $0 {predicate-source-dir}"
+SOURCE_DIR=$(readlink -f $(dirname $0)/..)
+
+if [ ! -d "${SOURCE_DIR}" ] ; then
+    echo "Predicate source directory not found"
     exit 1
 fi
 
-if [ -z "$PREDICATE_INSTALL_PREFIX" ] ; then
-    echo "Please set \$PREDICATE_INSTALL_PREFIX"
+if [ -z "${PREDICATE_INSTALL_PREFIX}" ] ; then
+    echo "Please set \$PREDICATE_INSTALL_PREFIX, e.g. to \$HOME/predicate/inst"
     exit 1
 fi
 
@@ -37,17 +48,18 @@ else
     _libdir=lib
 fi
 
-mkdir -p $PREDICATE_INSTALL_PREFIX
+mkdir -p "${PREDICATE_INSTALL_PREFIX}"
 
-cmd="cmake $1 -DCMAKE_BUILD_TYPE=DebugFull -DCMAKE_INSTALL_PREFIX=$PREDICATE_INSTALL_PREFIX \
-    -DLIB_INSTALL_DIR=$PREDICATE_INSTALL_PREFIX/$_libdir -DPLUGIN_INSTALL_DIR=plugins \
-    -DQT_PLUGINS_DIR=$PREDICATE_INSTALL_PREFIX/$_libdir/plugins"
+cmd="cmake $* -DCMAKE_INSTALL_PREFIX=${PREDICATE_INSTALL_PREFIX} \
+    -DLIB_INSTALL_DIR=${PREDICATE_INSTALL_PREFIX}/${_libdir} \
+    -DPLUGIN_INSTALL_DIR=${PREDICATE_INSTALL_PREFIX}/${_libdir}/plugins \
+    -DQT_PLUGINS_DIR=${PREDICATE_INSTALL_PREFIX}/${_libdir}/plugins ${SOURCE_DIR}"
 
 echo "------------------------------------------"
 echo "CMake will be executed using this command:"
 echo
 echo $cmd
-if [ "$1" == "--dry-run" -o "$2" == "--dry-run" ] ; then
+if [ "$1" = "--dry-run" ] ; then
     exit 0
 fi
 echo
