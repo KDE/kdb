3a7cd7d28f09a0f97f1caabb941d5416a2381a97
Jaroslaw Staniek <staniek@kde.org>
2014-11-18 21:42:56 +0100

    improve API docs of QuerySchema
    GIT_SILENT

--- src/QuerySchema.h
+++ src/QuerySchema.h
@@ -507,18 +507,16 @@ public:
     */
     int tablePositionForAlias(const QString& name) const;
 
-    /*! @return table position (within FROM section) for @a tableName.
+    /*! @return position (within the FROM section) of table @a tableName.
      -1 is returend if there's no such table declared in the FROM section.
      @see tablePositions()
     */
     int tablePosition(const QString& tableName) const;
 
-    /*! @return a list of all @a tableName table occurrences (within FROM section).
-     E.g. for "SELECT * FROM table t, table t2" [0, 1] list is returned.
-     Empty list is returned there's no such table declared
-     in the FROM section at all.
-     @see tablePosition()
-    */
+    /*! @return a list of all occurrences of table @a tableName (within the FROM section).
+     E.g. for "SELECT * FROM table t, table t2" tablePositions("table") returns {0, 1} list.
+     Empty list is returned if there's no table @a tableName used in the FROM section at all.
+     @see tablePosition() */
     QList<int> tablePositions(const QString& tableName) const;
 
     /*! Provided for convenience.
