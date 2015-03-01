d58b8e1dbccce7fa52663e8043ff10d6b65a8ba8
Jaroslaw Staniek <staniek@kde.org>
2015-03-01 01:19:34 +0100

    /bin/sh wouldn't support functions, so don't use them
    (from calligra 2.9)

--- src/parser/generate_parser_code.sh
+++ src/parser/generate_parser_code.sh
@@ -17,17 +17,14 @@ bool parseData(Predicate::Parser *p, const char *data);
 const char* tokenName(unsigned int offset);
 unsigned int maxToken();' > SqlParser.h
 
-function fixWhitespace() {
-    sed --in-place 's/[[:space:]]\+$//;s/\t/        /g' $1
-}
 cat SqlParser.tab.h >> SqlParser.h
 echo '#endif' >> SqlParser.h
-fixWhitespace SqlParser.h
+sed --in-place 's/[[:space:]]\+$//;s/\t/        /g' SqlParser.h
 
 cat SqlParser.tab.c | sed -e "s/SqlParser\.tab\.c/SqlParser.cpp/g" > SqlParser.cpp
 echo 'const char* tokenName(unsigned int offset) { return yytname[YYTRANSLATE(offset)]; }
 unsigned int maxToken() { return YYMAXUTOK; }' >> SqlParser.cpp
-fixWhitespace SqlParser.cpp
+sed --in-place 's/[[:space:]]\+$//;s/\t/        /g' SqlParser.cpp
 
 ./extract_tokens.sh > tokens.cpp
 rm -f SqlParser.tab.h SqlParser.tab.c
