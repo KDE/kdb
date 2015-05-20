7afdaa634eeba1dfa66d4c5b374bcdb9d8053cb3
Jaroslaw Staniek <staniek@kde.org>
2015-05-21 01:20:42 +0200

    SQL parser: check minimum version of flex and bison
    Using newer bison improves our quality.
    The requirement isn't a problem since we're shipping the generated code
    so only KDb developers need the access to the new bison/flex.
    
    (I am personally building bison on my own, what is a simple task)
    
    Based on Calligra 2.9 47bcfe60702f2

--- src/parser/generate_parser_code.sh
+++ src/parser/generate_parser_code.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
-# generates parser and lexer code using bison and flex
+# Generates parser and lexer code using bison and flex
 
+BISON_MIN=3.0.4      # keep updated for best results
+BISON_MIN_NUM=30004  # keep updated for best results
+FLEX_MIN=2.5.37      # keep updated for best results
+FLEX_MIN_NUM=20537   # keep updated for best results
+
+# Check minimum version of bison
+bisonv=`bison --version | head -n 1| cut -f4 -d" "`
+bisonv1=`echo $bisonv | cut -f1 -d.`
+bisonv2=`echo $bisonv | cut -f2 -d.`
+bisonv3=`echo $bisonv | cut -f3 -d.`
+if [ -z $bisonv3 ] ; then bisonv3=0; fi
+bisonvnum=`expr $bisonv1 \* 10000 + $bisonv2 \* 100 + $bisonv3`
+if [ $bisonvnum -lt $BISON_MIN_NUM ] ; then
+    echo "$bisonv is too old bison version, the minimum is $BISON_MIN."
+    exit 1
+fi
+
+# Check minimum version of flex
+flexv=`flex --version | head -n 1| cut -f2 -d" "`
+flexv1=`echo $flexv | cut -f1 -d.`
+flexv2=`echo $flexv | cut -f2 -d.`
+flexv3=`echo $flexv | cut -f3 -d.`
+flexvnum=`expr $flexv1 \* 10000 + $flexv2 \* 100 + $flexv3`
+if [ $flexvnum -lt $FLEX_MIN_NUM ] ; then
+    echo "$flexv is too old flex version, the minimum is $FLEX_MIN."
+    exit 1
+fi
+
+# Generate lexer and parser
 builddir=$PWD
 srcdir=`dirname $0`
 cd $srcdir
 
-# check version of bison
-read a b c <<< $(bison --version | head -n 1 | sed "s/.* \([0-9]\.[0-9].*\)/\\1/;s/\./\n/g")
-v=$(expr $a \* 10000 + $b \* 100 + $c)
-if [ $v -lt 30000 ] ; then echo "Error: bison >= 3.0.0 is required, found $a.$b.$c" ; exit 1; fi
-
-# generate lexer and parser
 flex -ogenerated/sqlscanner.cpp KDbSqlScanner.l
 bison -d KDbSqlParser.y -Wall -fall -rall -t --report-file=$builddir/KDbSqlParser.output
 
@@ -25,6 +48,7 @@ bool parseData(KDbParser *p, const char *data);
 KDB_TESTING_EXPORT const char* tokenName(unsigned int offset);
 KDB_TESTING_EXPORT unsigned int maxToken();' > generated/sqlparser.h
 
+# Fine-tune the code: extra functions and remove trailing white space
 cat KDbSqlParser.tab.h >> generated/sqlparser.h
 echo '#endif' >> generated/sqlparser.h
 sed --in-place 's/[[:space:]]\+$//;s/\t/        /g' generated/sqlparser.h
@@ -34,5 +58,6 @@ echo 'KDB_TESTING_EXPORT const char* tokenName(unsigned int offset) { return yyt
 KDB_TESTING_EXPORT unsigned int maxToken() { return YYMAXUTOK; }' >> generated/sqlparser.cpp
 sed --in-place 's/[[:space:]]\+$//;s/\t/        /g' generated/sqlparser.cpp
 
+# Extract table of SQL tokens
 ./extract_tokens.sh > generated/tokens.cpp
 rm -f KDbSqlParser.tab.h KDbSqlParser.tab.c
