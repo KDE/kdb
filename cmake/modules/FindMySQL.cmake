b757960216fb915ec1f46b2aceab129fb4b71b43
Jaroslaw Staniek <staniek@kde.org>
2016-07-25 18:56:52 +0200

    Improve FindMySQL

--- cmake/modules/FindMySQL.cmake
+++ cmake/modules/FindMySQL.cmake
@@ -6,8 +6,8 @@
 #  MYSQL_LIB_DIR, path to the MYSQL_LIBRARIES
 #  MYSQL_EMBEDDED_LIBRARIES, the libraries needed to use MySQL Embedded.
 #  MYSQL_EMBEDDED_LIB_DIR, path to the MYSQL_EMBEDDED_LIBRARIES
-#  MYSQL_FOUND, If false, do not try to use MySQL.
-#  MYSQL_EMBEDDED_FOUND, If false, do not try to use MySQL Embedded.
+#  MySQL_FOUND, If false, do not try to use MySQL.
+#  MySQL_Embedded_FOUND, If false, do not try to use MySQL Embedded.
 
 # Copyright (c) 2006-2008, Jarosław Staniek <staniek@kde.org>
 #
@@ -29,12 +29,19 @@ if(WIN32)
       $ENV{ProgramFiles}/*/include # MariaDB
    )
 else()
+   # use pkg-config to get the directories and then use these values
+   # in the FIND_PATH() and FIND_LIBRARY() calls
+   find_package(PkgConfig)
+   pkg_check_modules(PC_MYSQL QUIET mysql)
+
    find_path(MYSQL_INCLUDE_DIR mysql.h
       PATHS
       $ENV{MYSQL_INCLUDE_DIR}
       $ENV{MYSQL_DIR}/include
       /usr/local/mysql/include
       /opt/mysql/mysql/include
+      ${PC_MYSQL_INCLUDEDIR}
+      ${PC_MYSQL_INCLUDE_DIRS}
       PATH_SUFFIXES
       mysql
    )
@@ -73,16 +80,17 @@ if(WIN32)
    )
    set(MYSQL_LIBRARIES ${_LIBMYSQL_LIBRARY} ${_MYSQLCLIENT_LIBRARY})
 else()
-   find_library(_LIBMYSQL_LIBRARY NAMES libmysql
+   find_library(_MYSQLCLIENT_LIBRARY NAMES mysqlclient
       PATHS
       $ENV{MYSQL_DIR}/libmysql_r/.libs
       $ENV{MYSQL_DIR}/lib
       $ENV{MYSQL_DIR}/lib/mysql
-      /usr/local/mysql/lib
-      /opt/mysql/mysql/lib
+      ${PC_MYSQL_LIBDIR}
+      ${PC_MYSQL_LIBRARY_DIRS}
       PATH_SUFFIXES
       mysql
    )
+   set(MYSQL_LIBRARIES ${_MYSQLCLIENT_LIBRARY})
 endif()
 
 find_library(MYSQL_EMBEDDED_LIBRARIES NAMES mysqld
@@ -92,6 +100,7 @@ find_library(MYSQL_EMBEDDED_LIBRARIES NAMES mysqld
 
 if(_LIBMYSQL_LIBRARY)
    get_filename_component(MYSQL_LIB_DIR ${_LIBMYSQL_LIBRARY} PATH)
+   unset(_LIBMYSQL_LIBRARY)
 endif()
 
 if(MYSQL_EMBEDDED_LIBRARIES)
@@ -104,20 +113,15 @@ set( CMAKE_REQUIRED_LIBRARIES ${MYSQL_EMBEDDED_LIBRARIES} )
 check_cxx_source_compiles( "#include <mysql.h>\nint main() { int i = MYSQL_OPT_USE_EMBEDDED_CONNECTION; }" HAVE_MYSQL_OPT_EMBEDDED_CONNECTION )
 macro_pop_required_vars()
 
-if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES)
-   set(MYSQL_FOUND TRUE)
-   message(STATUS "Found MySQL: ${MYSQL_INCLUDE_DIR}, ${MYSQL_LIBRARIES}")
-else()
-   set(MYSQL_FOUND FALSE)
-   message(STATUS "MySQL not found.")
-endif()
+# Did we find anything?
+include(FindPackageHandleStandardArgs)
 
-if(MYSQL_INCLUDE_DIR AND MYSQL_EMBEDDED_LIBRARIES AND HAVE_MYSQL_OPT_EMBEDDED_CONNECTION)
-   set(MYSQL_EMBEDDED_FOUND TRUE)
-   message(STATUS "Found MySQL Embedded: ${MYSQL_INCLUDE_DIR}, ${MYSQL_EMBEDDED_LIBRARIES}")
-else()
-   set(MYSQL_EMBEDDED_FOUND FALSE)
-   message(STATUS "MySQL Embedded not found.")
-endif()
+find_package_handle_standard_args(MySQL
+                                  REQUIRED_VARS MYSQL_LIBRARIES MYSQL_INCLUDE_DIR MYSQL_LIB_DIR)
+find_package_handle_standard_args(MySQL_Embedded
+                                  REQUIRED_VARS MYSQL_EMBEDDED_LIBRARIES MYSQL_INCLUDE_DIR
+                                                MYSQL_EMBEDDED_LIB_DIR
+                                                HAVE_MYSQL_OPT_EMBEDDED_CONNECTION)
 
-mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES MYSQL_EMBEDDED_LIBRARIES)
+mark_as_advanced(MYSQL_INCLUDE_DIR MYSQL_LIBRARIES MYSQL_LIB_DIR
+                 MYSQL_EMBEDDED_LIBRARIES MYSQL_EMBEDDED_LIB_DIR HAVE_MYSQL_OPT_EMBEDDED_CONNECTION)
