30c1bc63f5c0c32017332267c22d06c3cac81050
Jaroslaw Staniek <staniek@kde.org>
2017-06-23 23:56:30 +0200

    update_recommended_sqlite_ver.sh: versions at http://sqlite.org/news.html are not fully sorted, sort them and take the newest one

--- tools/update_recommended_sqlite_ver.sh
+++ tools/update_recommended_sqlite_ver.sh
@@ -3,8 +3,8 @@ set -e
 
 $(git diff --quiet && git diff --cached --quiet) || (echo "There are repo changes, giving up."; exit 1)
 
-ver=`curl -s http://sqlite.org/news.html | grep '^<a name.*</a><h3>.* - Release ' | \
-    sed -e 's/.* - Release \(.*\)<\/.*/\1/'`
+ver=`curl -s http://sqlite.org/news.html | grep '<a name.*</a><h3>.* - Release ' | \
+    sed -e 's/.* - Release \(.*\)<\/.*/\1/' | sort -r | head -n1`
 file=src/drivers/CMakeLists.txt
 perl -pi -e "s/^set\(SQLITE_RECOMMENDED_VERSION.*/set\(SQLITE_RECOMMENDED_VERSION $ver\)/" $file
 git diff --quiet && (echo "Already updated to $ver."; exit 1)
